f703cf61eb78fa04f00cbcd9cff70a2323872419
NITHIN NAYAK <nithinnayak0721@gmail.com>
2025-10-21 14:36:26 +0530

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,12 @@
+(Windows build script)
+@echo off
+javac HelloWorld.java
+if %errorlevel% neq 0 (
+echo Build failed!
+exit /b %errorlevel%
+)
+echo Build successful.
+java HelloWorld
+Test locally by double-clicking build.bat. It should print:
+Build successful.
+Hello, Jenkins!
